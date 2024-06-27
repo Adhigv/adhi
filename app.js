4a39a10308383bd69120e71e9ee3b9042be05b5a
Adhigv <adarshagv7@gmail.com>
2024-06-27 15:34:05 +0530

    nhsjh

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app =express();
 
 app.get('/gagan',(req,res)=>{
-    res.send("welcome to the website");
+    res.sendFile(__dirname+"/index.html");
 });
 app.listen(4000,()=>{
     console.log("listening to port 4000");
